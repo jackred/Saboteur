73a41504f1146fd02edb6f70304d01cee6bd8ccd
jackred <jackred@tutamail.com>
2019-06-16 18:16:37 +0200

    TEST build for the test bot

--- start.sh
+++ start.sh
@@ -5,12 +5,15 @@ if [ $# -ne 0 ];
 then
     if [ $1 = "debug" ];
     then
-    sudo docker run --rm -v $here/config.json:$src/config.json --log-opt max-size=10m -it --name saboteur saboteur
+    docker run --rm -v $here/config.json:$src/config.json --log-opt max-size=10m --net ng_network -it --name saboteur saboteur
+    elif [ $1 = "test" ];
+    then
+    docker run --rm -v $here/config.test.json:$src/config.json --log-opt max-size=10m --net ng_network -it --name test test
     else
 	echo "wrong argument"
     fi
 else
-    sudo docker run -v $here/config.json:$src/config.json --log-opt max-size=10m --restart always -dit --name saboteur saboteur
+    docker run -v $here/config.json:$src/config.json --log-opt max-size=10m --restart always --net ng_network -dit --name saboteur saboteur
 fi
  
 
